866ceb464cdb9f7f2ccb7cd87520d081ac6bb1a4
Chris Shenton <chris@v-studios.com>
2017-05-15 17:29:07 -0400

    Remove nonessential code, improve comments

--- binaryapi.py
+++ binaryapi.py
@@ -1,80 +1,58 @@
+# Demonstrate binary in/out for Lambda fronted by API Gateway:
+# take an incoming GIF, convert to JPEG and return it.
+#
+# This expects to be called with a binary payload and headers to cause APIG to
+# do binary/string conversion to/from Lambda.::
+#
+#     curl -H "Content-Type: image/gif" -H "Accept: image/jpeg" \
+#          --data-binary @CSLOGO.gif $API
+#
+# You must set APIG Binary Support for the content types in AWS Console.
+
 from base64 import b64decode, b64encode
-import json
-import logging
-import subprocess
-
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger()
-log.setLevel(logging.INFO)
-
-log.info('Outside of handler')
-
-# I'm gonna expect to be called with only a data payload, e.g.,
-# curl -v --binary-data @CSLOGO.gif $API
-
-
-def run(cmd):
-    """Run a command as a subprocess, log output or errors."""
-    log.info('RUN {}'.format(cmd))
-    if type(cmd) is str:
-        cmd = cmd.split()
-    try:
-        log.info('RAN {}: {}'.format(cmd, subprocess.check_output(cmd)))
-    except subprocess.CalledProcessError as e:
-        log.error('RAN {}: {}'.format(cmd, e))
-        raise
+from json import dumps
+from subprocess import check_output
 
 
 def handler(event, context):
-    log.info('API Event: {}'.format(json.dumps(event)))
-    body = event['body']
-    log.info('API isBase64Encoded={}'.format(event['isBase64Encoded']))
-    log.info('body type={}'.format(type(body)))
-    # log.info('body:\n{}'.format(body))
-    log.info('body len: {}'.format(len(body)))
-    log.info('content-type: {}'.format(event['headers']['Content-Type']))
+    # The data should have been sent with Content-Type that the APIG recognizes
+    # as binary so it will base64-encode it to us. Decode and save image.
 
-    log.info('b64decoding...')
-    img = b64decode(body)
-    log.info('writing /tmp/img.gif...')
+    if event['headers']['Content-Type'] != 'image/gif':
+        return {'statusCode': 400,
+                'body': dumps({'message': 'Content-Type must be image/gif'})}
+    if event['isBase64Encoded'] is False:
+        return {'statusCode': 400,
+                'body': dumps({'message': 'APIG did not provide base64 data'})}
+
+    img_gif = b64decode(event['body'])
     with open('/tmp/img.gif', 'wb') as img_fp:
-        img_fp.write(img)
-    run('ls -l /tmp')
-    log.info('converting...')
-    run('convert /tmp/img.gif /tmp/img.jpg')
-    run('ls -l /tmp')
+        img_fp.write(img_gif)
 
-    # Base64 encode the binary and convert those bytes to UTF string for JSON body
-    # The request must set the header "Accept: image/jpeg" for the APIG to turn
-    # the byte string in to binary, and the Binary 
-    log.info('reading output JPG and b64 encodeing...')
+    # Do the conversion here (whatever your app needs, could be a thumbnailer)
+
+    check_output(['convert', '/tmp/img.gif', '/tmp/img.jpg'])
+
+    # To return binary, we must Base64 encode the output image and convert
+    # those bytes to UTF string for JSON body.  The request must set the header
+    # "Accept: image/jpeg" for the APIG to turn the encoded string in to binary
+    # for the client HTTP response.
+
+    if event['headers']['Accept'] != 'image/jpeg':
+        return {'statusCode': 400,
+                'body': dumps({'message': 'Accept must specify image/jpeg'})}
+
     img_jpg = open('/tmp/img.jpg', 'rb').read()
     out_b64 = b64encode(img_jpg)
     out_str = out_b64.decode('utf8')
-    log.info('out_str: {}'.format(out_str))
 
-    # Lamda proxy specifies: statusCode, isBase64Encoded, body, headers
+    # Set status and return output as JSON string which APIG converts to binary
+    # APIG ignores the Content-Type here, only looking at client Accept header
+    # but we want to tell the final client the type.
+
     return {
         'statusCode': 200,
         'body': out_str,
         'isBase64Encoded': True,
         'headers':  {'Content-Type': 'image/jpeg'},
     }
-    # # Code for default http lambda-proxy integration
-    # out = {
-    #     "message": "OK",
-    #     "bodyLen": len(body),
-    #     "event": event
-    # }
-    # return {
-    #     "statusCode": 200,
-    #     "body": json.dumps(out)
-    # }
-
-    # Use this if you don't use the http event with LAMBDA-PROXY integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
